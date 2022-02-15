5f3bf13c2290304d5152dca64dddcae83d737e50
KG O'Hearon <ohearonkg@gmail.com>
2022-02-15 18:38:50 -0400

    feat: show basic inline example

---
+++ packages/inline-consumer-example/src/App.tsx
@@ -0,0 +1,407 @@
+import React from "react";
+import "./App.css";
+
+export default function App() {
+  const [state, setState] = React.useState(false);
+  return (
+    <>
+      <h1> Demo </h1>
+      <button onClick={() => setState((state) => !state)}>Click Me </button>
+      <hr />
+
+      <object type="image/svg+xml" data="/icons/bag.svg">
+        Something
+      </object>
+      <object type="image/svg+xml" data="/icons/alarm-fill.svg">
+        Something
+      </object>
+      // <object type="image/svg+xml" data="/icons/alarm.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/align-bottom.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/align-center.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/align-end.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/align-middle.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/align-start.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/align-top.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/alt.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/app-indicator.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/apple.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/App.css">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/archive-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/archive-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-90deg-down.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-90deg-left.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-90deg-right.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-90deg-up.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-bar-down.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-bar-left.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-bar-right.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-bar-up.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-clockwise.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-counterclockwise.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-circle-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-circle.svg">
+      //   Something
+      // </object>
+      // <object
+      //   type="image/svg+xml"
+      //   data="/icons/arrow-down-left-circle-fill.svg"
+      // >
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-left-circle.svg">
+      //   Something
+      // </object>
+      // <object
+      //   type="image/svg+xml"
+      //   data="/icons/arrow-down-left-square-fill.svg"
+      // >
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-left-square.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-left.svg">
+      //   Something
+      // </object>
+      // <object
+      //   type="image/svg+xml"
+      //   data="/icons/arrow-down-right-circle-fill.svg"
+      // >
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-right-circle.svg">
+      //   Something
+      // </object>
+      // <object
+      //   type="image/svg+xml"
+      //   data="/icons/arrow-down-right-square-fill.svg"
+      // >
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-right-square.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-right.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-short.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-square-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-square.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-down-up.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-left-circle-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-left-circle.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-left-right.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-left-short.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-left-square-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-left-square.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-left.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-repeat.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-return-left.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-return-right.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-right-circle-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-right-circle.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-right-short.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-right-square-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-right-square.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-right.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrows-angle-contract.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrows-angle-expand.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrows-collapse.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrows-expand.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrows-fullscreen.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrows-move.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-through-heart-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-through-heart.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-circle-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-circle.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-left-circle-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-left-circle.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-left-square-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-left-square.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-left.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-right-circle-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-right-circle.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-right-square-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-right-square.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-right.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-short.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-square-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up-square.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/arrow-up.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/aspect-ratio-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/aspect-ratio.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/asterisk.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/at.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/award-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/award-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/backspace-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/backspace-reverse-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/backspace-reverse.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/backspace-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/back.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-3d-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-3d.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-4k-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-4k.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-8k-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-8k.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-ad-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-ad.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-ar-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-ar.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-cc-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-cc.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-hd-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-hd.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-sd-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-sd.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-tm-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-tm.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-vo-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-vo.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-vr-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-vr.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-wc-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/badge-wc.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-check-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-check.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-dash-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-dash.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-heart-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-heart.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-plus-fill.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-plus.svg">
+      //   Something
+      // </object>
+      // <object type="image/svg+xml" data="/icons/bag-check-fill.svg">
+      //   Something
+      // </object>
+    // </>
+  );
+}
