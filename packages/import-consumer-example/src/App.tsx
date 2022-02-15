e3294958a861db9b54e20345e92a1cd4af64b30c
KG O'Hearon <ohearonkg@gmail.com>
2022-02-15 18:17:34 -0400

    chore: update to relevant name

---
+++ packages/import-consumer-example/src/App.tsx
@@ -0,0 +1,523 @@
+import React from "react";
+import "./App.css";
+
+import activity from "./icons/activity.svg";
+import alarmFill from "./icons/alarm-fill.svg";
+import alarmfrom from "./icons/alarm.svg";
+import alignbottom from "./icons/align-bottom.svg";
+import aligncenter from "./icons/align-center.svg";
+import alignend from "./icons/align-end.svg";
+import alignmiddle from "./icons/align-middle.svg";
+import alignstart from "./icons/align-start.svg";
+import aligntop from "./icons/align-top.svg";
+import alt from "./icons/alt.svg";
+import appindicator from "./icons/app-indicator.svg";
+import apple from "./icons/apple.svg";
+import app from "./icons/app.svg";
+import archivefill from "./icons/archive-fill.svg";
+import archive from "./icons/archive.svg";
+import arrow90degdown from "./icons/arrow-90deg-down.svg";
+import arrow90degleft from "./icons/arrow-90deg-left.svg";
+import arrow90degright from "./icons/arrow-90deg-right.svg";
+import arrow90degup from "./icons/arrow-90deg-up.svg";
+import arrowbardown from "./icons/arrow-bar-down.svg";
+import arrowbarleft from "./icons/arrow-bar-left.svg";
+import arrowbarright from "./icons/arrow-bar-right.svg";
+import arrowbarup from "./icons/arrow-bar-up.svg";
+import arrowclockwise from "./icons/arrow-clockwise.svg";
+import arrowcounterclockwise from "./icons/arrow-counterclockwise.svg";
+import arrowdowncirclefill from "./icons/arrow-down-circle-fill.svg";
+import arrowdowncircle from "./icons/arrow-down-circle.svg";
+import arrowdownleftcirclefill from "./icons/arrow-down-left-circle-fill.svg";
+import arrowdownleftcircle from "./icons/arrow-down-left-circle.svg";
+import arrowdownleftsquarefill from "./icons/arrow-down-left-square-fill.svg";
+import arrowdownleftsquare from "./icons/arrow-down-left-square.svg";
+import arrowdownleft from "./icons/arrow-down-left.svg";
+import arrowdownrightcirclefill from "./icons/arrow-down-right-circle-fill.svg";
+import arrowdownrightcircle from "./icons/arrow-down-right-circle.svg";
+import arrowdownrightsquarefill from "./icons/arrow-down-right-square-fill.svg";
+import arrowdownrightsquare from "./icons/arrow-down-right-square.svg";
+import arrowdownright from "./icons/arrow-down-right.svg";
+import arrowdownshort from "./icons/arrow-down-short.svg";
+import arrowdownsquarefill from "./icons/arrow-down-square-fill.svg";
+import arrowdownsquare from "./icons/arrow-down-square.svg";
+import arrowdown from "./icons/arrow-down.svg";
+import arrowdownup from "./icons/arrow-down-up.svg";
+import arrowleftcirclefill from "./icons/arrow-left-circle-fill.svg";
+import arrowleftcircle from "./icons/arrow-left-circle.svg";
+import arrowleftright from "./icons/arrow-left-right.svg";
+import arrowleftshort from "./icons/arrow-left-short.svg";
+import arrowleftsquarefill from "./icons/arrow-left-square-fill.svg";
+import arrowleftsquare from "./icons/arrow-left-square.svg";
+import arrowleft from "./icons/arrow-left.svg";
+import arrowrepeat from "./icons/arrow-repeat.svg";
+import arrowreturnleft from "./icons/arrow-return-left.svg";
+import arrowreturnright from "./icons/arrow-return-right.svg";
+import arrowrightcirclefill from "./icons/arrow-right-circle-fill.svg";
+import arrowrightcircle from "./icons/arrow-right-circle.svg";
+import arrowrightshort from "./icons/arrow-right-short.svg";
+import arrowrightsquarefill from "./icons/arrow-right-square-fill.svg";
+import arrowrightsquare from "./icons/arrow-right-square.svg";
+import arrowright from "./icons/arrow-right.svg";
+import arrowsanglecontract from "./icons/arrows-angle-contract.svg";
+import arrowsangleexpand from "./icons/arrows-angle-expand.svg";
+import arrowscollapse from "./icons/arrows-collapse.svg";
+import arrowsexpand from "./icons/arrows-expand.svg";
+import arrowsfullscreen from "./icons/arrows-fullscreen.svg";
+import arrowsmove from "./icons/arrows-move.svg";
+import arrowthroughheartfill from "./icons/arrow-through-heart-fill.svg";
+import arrowthroughheart from "./icons/arrow-through-heart.svg";
+import arrowupcirclefill from "./icons/arrow-up-circle-fill.svg";
+import arrowupcircle from "./icons/arrow-up-circle.svg";
+import arrowupleftcirclefill from "./icons/arrow-up-left-circle-fill.svg";
+import arrowupleftcircle from "./icons/arrow-up-left-circle.svg";
+import arrowupleftsquarefill from "./icons/arrow-up-left-square-fill.svg";
+import arrowupleftsquare from "./icons/arrow-up-left-square.svg";
+import arrowupleft from "./icons/arrow-up-left.svg";
+import arrowuprightcirclefill from "./icons/arrow-up-right-circle-fill.svg";
+import arrowuprightcircle from "./icons/arrow-up-right-circle.svg";
+import arrowuprightsquarefill from "./icons/arrow-up-right-square-fill.svg";
+import arrowuprightsquare from "./icons/arrow-up-right-square.svg";
+import arrowupright from "./icons/arrow-up-right.svg";
+import arrowupshort from "./icons/arrow-up-short.svg";
+import arrowupsquarefill from "./icons/arrow-up-square-fill.svg";
+import arrowupsquare from "./icons/arrow-up-square.svg";
+import arrowup from "./icons/arrow-up.svg";
+import aspectratiofill from "./icons/aspect-ratio-fill.svg";
+import aspectratio from "./icons/aspect-ratio.svg";
+import asterisk from "./icons/asterisk.svg";
+import at from "./icons/at.svg";
+import awardfill from "./icons/award-fill.svg";
+import award from "./icons/award.svg";
+import backspacefill from "./icons/backspace-fill.svg";
+import backspacereversefill from "./icons/backspace-reverse-fill.svg";
+import backspacereverse from "./icons/backspace-reverse.svg";
+import backspace from "./icons/backspace.svg";
+import back from "./icons/back.svg";
+import badge3dfill from "./icons/badge-3d-fill.svg";
+import badge3d from "./icons/badge-3d.svg";
+import badge4kfill from "./icons/badge-4k-fill.svg";
+import badge4k from "./icons/badge-4k.svg";
+import badge8kfill from "./icons/badge-8k-fill.svg";
+import badge8k from "./icons/badge-8k.svg";
+import badgeadfill from "./icons/badge-ad-fill.svg";
+import badgead from "./icons/badge-ad.svg";
+import badgearfill from "./icons/badge-ar-fill.svg";
+import badgear from "./icons/badge-ar.svg";
+import badgeccfill from "./icons/badge-cc-fill.svg";
+import badgecc from "./icons/badge-cc.svg";
+import badgehdfill from "./icons/badge-hd-fill.svg";
+import badgehd from "./icons/badge-hd.svg";
+import badgesdfill from "./icons/badge-sd-fill.svg";
+import badgesd from "./icons/badge-sd.svg";
+import badgetmfill from "./icons/badge-tm-fill.svg";
+import badgetm from "./icons/badge-tm.svg";
+import badgevofill from "./icons/badge-vo-fill.svg";
+import badgevo from "./icons/badge-vo.svg";
+import badgevrfill from "./icons/badge-vr-fill.svg";
+import badgevr from "./icons/badge-vr.svg";
+import badgewcfill from "./icons/badge-wc-fill.svg";
+import badgewc from "./icons/badge-wc.svg";
+import bagcheckfill from "./icons/bag-check-fill.svg";
+import bagcheck from "./icons/bag-check.svg";
+import bagdashfill from "./icons/bag-dash-fill.svg";
+import bagdash from "./icons/bag-dash.svg";
+import bagfill from "./icons/bag-fill.svg";
+import bagheartfill from "./icons/bag-heart-fill.svg";
+import bagheart from "./icons/bag-heart.svg";
+import bagplusfill from "./icons/bag-plus-fill.svg";
+import bagplus from "./icons/bag-plus.svg";
+import bag from "./icons/bag.svg";
+
+export default function App() {
+  const [state, setState] = React.useState(false);
+  return (
+    <>
+      <h1> Demo </h1>
+      <button onClick={() => setState((state) => !state)}>Click Me </button>
+      <hr />
+
+      <object type="image/svg+xml" data={activity}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={alarmFill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={alarmfrom}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={alignbottom}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={aligncenter}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={alignend}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={alignmiddle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={alignstart}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={aligntop}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={alt}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={appindicator}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={apple}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={app}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={archivefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={archive}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrow90degdown}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrow90degleft}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrow90degright}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrow90degup}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowbardown}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowbarleft}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowbarright}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowbarup}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowclockwise}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowcounterclockwise}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdowncirclefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdowncircle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownleftcirclefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownleftcircle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownleftsquarefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownleftsquare}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownleft}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownrightcirclefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownrightcircle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownrightsquarefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownrightsquare}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownright}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownshort}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownsquarefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownsquare}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdown}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowdownup}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowleftcirclefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowleftcircle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowleftright}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowleftshort}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowleftsquarefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowleftsquare}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowleft}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowrepeat}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowreturnleft}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowreturnright}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowrightcirclefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowrightcircle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowrightshort}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowrightsquarefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowrightsquare}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowright}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowsanglecontract}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowsangleexpand}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowscollapse}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowsexpand}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowsfullscreen}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowsmove}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowthroughheartfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowthroughheart}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupcirclefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupcircle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupleftcirclefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupleftcircle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupleftsquarefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupleftsquare}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupleft}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowuprightcirclefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowuprightcircle}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowuprightsquarefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowuprightsquare}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupright}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupshort}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupsquarefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowupsquare}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={arrowup}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={aspectratiofill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={aspectratio}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={asterisk}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={at}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={awardfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={award}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={backspacefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={backspacereversefill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={backspacereverse}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={backspace}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={back}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badge3dfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badge3d}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badge4kfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badge4k}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badge8kfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badge8k}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgeadfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgead}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgearfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgear}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgeccfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgecc}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgehdfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgehd}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgesdfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgesd}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgetmfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgetm}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgevofill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgevo}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgevrfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgevr}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgewcfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={badgewc}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagcheckfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagcheck}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagdashfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagdash}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagheartfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagheart}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagplusfill}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bagplus}>
+        Something
+      </object>
+      <object type="image/svg+xml" data={bag}>
+        Something
+      </object>
+    </>
+  );
+}
